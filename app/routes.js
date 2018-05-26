0df6253b83b0d4324259cc9f83365f614ddf3868
ELW <macbookpro@MacBook-Pro-de-MACBOOK.local>
2018-05-26 13:53:21 -0700

    Login

--- app/routes.js
+++ app/routes.js
@@ -2,14 +2,20 @@
 module.exports = function(app, passport) {
     app.post('/login', function(req, res, next) {
       passport.authenticate('local-login', function(err, user, info) {
+        if (!user) { return res.json({"user": null}); }
+        else{
         return res.json({"user": user});
+    }
     })(req, res, next);
     });
 
     // process the signup form
     app.post('/signup', function(req, res, next) {
       passport.authenticate('local-signup', function(err, user, info) {
-        return res.json({"user": user});
+        if (!user) { return res.json({"user": null}); }
+        else{
+        return res.json({"result":true, "user": user});
+    }
     })(req, res, next);
     });
 };

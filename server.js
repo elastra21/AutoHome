344d8619a68aedbf7e3a801c9f5f7f1e8014304d
Emmanuel Lastra Williams <elw@Emmanuels-MacBook-Pro.local>
2019-03-03 02:09:30 -0800

    Commit message

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-//var mongoose = require('mongoose');
-//var passport = require('passport');
-//var bodyParser = require('body-parser');
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var bodyParser = require('body-parser');
 var configDB = require('./config/database.js');
-var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var ipaddress = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 // configuration ===============================================================
-//mongoose.connect(configDB.db(),{useMongoClient: true }); // connect to our database
+mongoose.connect(configDB.db(),{useMongoClient: true }); // connect to our database
 
-//require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
-//app.use(passport.initialize());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
 // routes ======================================================================
-//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+app.listen(port, ipaddress, function() {
+    console.log('The magic happens on ' +ipaddress+':'+ port);
+    console.log('Connected to MongoDB at: %s', configDB.db());
 });
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;

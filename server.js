3901f2ccbb2be78c48a6aee9765167de2d744e40
Emmanuel Lastra Williams <elw@Emmanuels-MacBook-Pro.local>
2019-03-03 02:58:09 -0800

    Commit message

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var configDB = require('./config/database.js');
+Object.assign=require('object-assign');
+var morgan  = require('morgan');
 var ipaddress = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 // configuration ===============================================================
+app.use(morgan('combined'))
 mongoose.connect(configDB.db(),{useMongoClient: true }); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration

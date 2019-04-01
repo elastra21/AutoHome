553b607e09967913f882a3ff3b78f2e2e8d11ea6
elastra21 <elastra@uabc.edu.mx>
2019-03-31 23:19:47 -0700

    a ver

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require("body-parser");
 const CONNECTION_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/login"; // Heroku server
 //var ipaddress = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 // configuration ===============================================================
 mongoose.connect(
@@ -32,6 +32,6 @@ require("./app/routes.js")(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log("Server running on http://%s:%s", ipaddress, port);
+console.log("Server running on http://%s:%s", port);
 
 module.exports = app;

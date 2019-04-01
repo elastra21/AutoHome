e3a0b78359ddfc786aad1a3dc4f9f5c387546adb
elastra21 <elastra@uabc.edu.mx>
2019-03-31 22:36:45 -0700

    cambios para Heroku

--- server.js
+++ server.js
@@ -2,31 +2,36 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var mongoose = require('mongoose');
-var passport = require('passport');
-var bodyParser = require('body-parser');
-var configDB = require('./config/database.js');
-var ipaddress = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+//var configDB = require("./config/database.js");
+const CONNECTION_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/login"; // Heroku server
+var ipaddress = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 // configuration ===============================================================
-mongoose.connect(configDB.db(),{useMongoClient: true }); // connect to our database
-app.engine('html', require('ejs').renderFile);
+mongoose.connect(
+  CONNECTION_URI,
+  { useMongoClient: true }
+); // connect to our database
+app.engine("html", require("ejs").renderFile);
 
-require('./config/passport')(passport); // pass passport for configuration
+require("./config/passport")(passport); // pass passport for configuration
 
 // set up our express application
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require("./app/routes.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port, ipaddress);
-console.log('Server running on http://%s:%s', ipaddress, port);
+app.listen(port);
+console.log("Server running on http://%s:%s", ipaddress, port);
 
-module.exports = app ;
+module.exports = app;
